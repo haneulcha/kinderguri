65697a893eea955d4fc7e38338918996f39c3bbf
haneul <zr420cha@gmail.com>
2020-07-23 19:49:27 +0900

    add server

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
-import Search from "./Search";
-import Map from "./Map";
-import List from "./List";
-import Scroll from "./Scroll";
+import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+import Main from "./Main";
 
 const App = () => {
-  const [list, setList] = useState([]);
-  const [mapMarking, setMapMarking] = useState();
-
   return (
-    <div className="parent">
-      <div className="header">
-        <div className="header-text">
-          <h1>경기도 구리시 유치원·어린이집</h1>
-        </div>
-      </div>
-      <Search setList={setList} setMapMarking={setMapMarking} />
-      <List list={list} markingFn={setMapMarking} />
-      <Map list={list} markerId={mapMarking} />
-
-      <Scroll />
-    </div>
+    <Router>
+      <Main path="/" />
+    </Router>
   );
 };
 
